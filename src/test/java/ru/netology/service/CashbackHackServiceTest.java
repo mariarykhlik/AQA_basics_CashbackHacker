f2435a30fc5444936ac748774b363bbd953dea81
mariarykhlik <maria.rykhlik@gmail.com>
2022-07-19 21:12:02 +0300

    JUnitPlatform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.jupiter.api.Assertions;
+
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
@@ -26,5 +28,26 @@ public class CashbackHackServiceTest {
         int expected = 100;
         int actual = service.remain(1900);
         assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainUnderBoundaryByJupiter() {
+        int expected = 100;
+        int actual = service.remain(900);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainEqualBoundaryByJupiter() {
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainOverBoundaryByJupiter() {
+        int expected = 100;
+        int actual = service.remain(1900);
+        Assertions.assertEquals(expected, actual);
     }
 }
